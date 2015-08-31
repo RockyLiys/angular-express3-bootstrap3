1b01381c3680624b91dcc4cef335f691716b0597
RockyLiys <liys_liys@163.com>
2015-08-31 10:52:15 +0800

    modify app.js

--- app.js
+++ app.js
@@ -4,20 +4,13 @@ var express = require('express')
     , app = express()
     , server = require('http').createServer(app);
 
-<<<<<<< HEAD
-
+// port
 app.set('port', process.env.PORT || 4000);
 //templates
 app.set('views', __dirname + '/views');
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html'); //修改文件扩展名ejs为html
 
-=======
-app.set('port', process.env.PORT || 4000);
-app.set('views', __dirname + '/views');
-app.engine('.html', ejs.__express);
-app.set('view engine', 'html'); //修改文件扩展名ejs为html
->>>>>>> 46d6bb774e300aa1de6f5ac96870e0359ff30a4f
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -34,11 +27,6 @@ app.get('/', function (req, res) {
     res.sendfile('app/index.html');
 });
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 46d6bb774e300aa1de6f5ac96870e0359ff30a4f
 server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
